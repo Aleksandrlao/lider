4359734585f026b2655dd14c0220eab276c70276
Aleksandrlao <aleksandrlao@mail.ru>
2017-04-21 01:28:32 +0300

    last verstka

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function($) {
 	
-
+// main top slider
+	$('.c_slider').slick({
+		dots: true, arrows: true, infinite: true, speed: 300, slidesToShow: 1, slidesToScroll: 1, autoplay: true, autoplaySpeed: 3000
+	});
 
 
 });
